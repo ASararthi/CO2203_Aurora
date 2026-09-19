5242e1782fb2b42c62f3ab2284e184b197967fac
thineli <thinelidanithma16@gmail.com>
2026-09-19 19:00:50 +0530

    changed repo

--- include/Repository.h
+++ include/Repository.h
@@ -1,8 +1,10 @@
 #ifndef REPOSITORY_H
 #define REPOSITORY_H
+
 #include <map>
 #include <vector>
 #include <string>
+#include <fstream>
 
 template <typename T>
 class Repository
@@ -13,6 +15,11 @@ protected:
 
 public:
     Repository() = default;
+
+    Repository(const std::string& filePath)
+        : FilePath(filePath)
+    {
+    }
 
     void add(T item);
     void remove(const std::string& id);
@@ -26,5 +33,23 @@ public:
 
     virtual ~Repository() = default;
 };
+
+template <typename T>
+void Repository<T>::save()
+{
+    std::ofstream file(FilePath);
+
+    if (!file)
+    {
+        return;
+    }
+
+    for (const auto& item : items)
+    {
+        file << *(item.second) << '\n';
+    }
+
+    file.close();
+}
 
 #endif
