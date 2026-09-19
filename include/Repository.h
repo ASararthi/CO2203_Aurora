1d054da0dffcb5f8c6fab1b8824117abe97ee63e
thineli <thinelidanithma16@gmail.com>
2026-09-19 15:11:32 +0530

    update repo

--- include/Repository.h
+++ include/Repository.h
@@ -1,27 +1,30 @@
 #ifndef REPOSITORY_H
 #define REPOSITORY_H
-
 #include <map>
-#include <string>
 #include <vector>
+#include <string>
 
 template <typename T>
 class Repository
 {
 protected:
     std::map<std::string, T> items;
-    std::string filePath;
+    std::string FilePath;
 
 public:
+    Repository() = default;
+
     void add(T item);
     void remove(const std::string& id);
+
     T findById(const std::string& id);
-    std::vector<T> findAll();
+
+    std::vector<T> findAll() const;
 
     void save();
     void load();
+
+    virtual ~Repository() = default;
 };
 
 #endif
-
-
