07e9e5626bb45d4b362f813116f9e37c1127c6bf
Agra <agrasamarasekara@gmail.com>
2026-09-19 13:01:27 +0530

    Changed attendance record

--- include/AttendanceRecord.h
+++ include/AttendanceRecord.h
@@ -1,29 +1,22 @@
 #ifndef ATTENDANCERECORD_H
 #define ATTENDANCERECORD_H
-
+#include "AttendanceSession.h"
 #include <iostream>
 #include <string>
 
 class AttendanceRecord
 {
 private:
-    std::string studentId;
-    std::string sessionId;
+    Student* student;
+    AttendanceSession* session;
     std::string timestamp;
     std::string status;
     std::string capturedVia;
     bool isCorrection;
-
+    Lecturer* correctingLecturer;
+    std::string correctionReason;
 public:
-    AttendanceRecord(const std::string& studentId,
-                     const std::string& sessionId,
-                     const std::string& timestamp,
-                     const std::string& status,
-                     const std::string& capturedVia,
-                     bool isCorrection = false);
-
-    friend std::ostream& operator<<(std::ostream& os,
-                                    const AttendanceRecord& record);
+    AttendanceRecord(Student* student, AttendanceSession* session);
+    friend std::ostream& operator<<(std::ostream& os, const AttendanceRecord& r);
 };
-
 #endif
