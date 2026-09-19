156517a8fd6750a4e42583507aae78cef4c029bf
thineli <thinelidanithma16@gmail.com>
2026-09-19 20:03:05 +0530

    Remove unused repository files

--- include/AttendanceCapture.h
+++ include/AttendanceCapture.h
@@ -1,17 +1,17 @@
 #ifndef ATTENDANCECAPTURE_H
 #define ATTENDANCECAPTURE_H
 
+#include <string>
+
 class AttendanceSession;
 
 class AttendanceCapture
 {
 public:
-    virtual ~AttendanceCapture() = default;
+    virtual ~AttendanceCapture();
 
     virtual void beginSession(AttendanceSession& session) = 0;
-
-    virtual void captureNext(AttendanceSession& session) = 0;
-
+    virtual std::string captureNext() = 0;
     virtual void endSession() = 0;
 };
 
