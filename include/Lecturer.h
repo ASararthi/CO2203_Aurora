826a215e346b1d472090b8e4f41aafaa25feaf79
Agra <agrasamarasekara@gmail.com>
2026-09-19 11:45:18 +0530

    Added class student and reason

--- include/Lecturer.h
+++ include/Lecturer.h
@@ -1,11 +1,16 @@
 #ifndef LECTURER_H
 #define LECTURER_H
 
+#include <vector>
 #include "Person.h"
 #include <string>
+#include "Course.h"
+#include "AttendanceSession.h"
 
 class Lecturer : public Person
 {
+private:
+    std::vector<Course*> assignedCourses;
 public:
     Lecturer(const std::string& id,
              const std::string& name,
@@ -13,6 +18,10 @@ public:
              const std::string& password);
 
     void showMenu() override;
-};
+
+    std::vector<Student*> viewEnrolmentList(const Course& course) const;
+    AttendanceSession* openAttendanceSession( const TimeSlot& slot, const std::string& sessionId, int durationMins);//why did we add session id?
+    void closeAttendanceSession(AttendanceSession& session);
+};  
 
 #endif
